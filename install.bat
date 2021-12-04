947f4d1888b6d6cf5f1a84d45294a4ef48b881cf
yebafan <adass5202@gmail.com>
2021-12-04 20:09:23 +0100

    fix installation from paths with spaces

--- install.bat
+++ install.bat
@@ -1,2 +1,2 @@
-reg add HKEY_LOCAL_MACHINE\SOFTWARE\Google\Chrome\NativeMessagingHosts\com.asmejkal.youtubedownloader /f /d %~dp0native_manifest.json
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Google\Chrome\NativeMessagingHosts\com.asmejkal.youtubedownloader /f /d "%~dp0native_manifest.json"
 dotnet build -c Release NativeHost/NativeHost.csproj
